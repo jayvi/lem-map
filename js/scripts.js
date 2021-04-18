4d02677c112ff77647f58bc356acc3512df953cf
jayvi <jayvii@jays-Mac-Pro-2.local>
2021-04-18 21:46:07 +0300

    FIx

--- js/scripts.js
+++ js/scripts.js
@@ -162,19 +162,7 @@ jQuery(document).ready(function ($) {
 			wateraudio.pause();
 			wateraudio.currentTime = 0;
 		});
-	//construction site
-		var constructTimeout
-		var constructaudio = $("#construction")[0];
-		$(".construction-site").mouseenter(function() {
-			constructTimeout = setTimeout(function() {
-				constructaudio.play();
-				constructaudio.volume = 0.5;
-			}, 800);
-		}).mouseleave(function() {
-			constructaudio.pause();
-			constructaudio.currentTime = 0;
-			clearTimeout(constructTimeout);
-		});
+	
 	//christ redeemer
 		var christaudio = $("#christ-redeemer")[0];
 		$(".christ-redeemer").mouseenter(function() {
@@ -183,74 +171,8 @@ jQuery(document).ready(function ($) {
 			christaudio.pause();
 			christaudio.currentTime = 0;
 		});
-	//church 1 
-		var churchoneaudio = $("#church-1")[0];
-		$(".church-1").mouseenter(function() {
-			churchoneaudio.play();
-		}).mouseleave(function() {
-			churchoneaudio.pause();
-			churchoneaudio.currentTime = 0;
-		});
-	//church 2 
-		var churchtwoaudio = $("#church-2")[0];
-		$(".church-2").mouseenter(function() {
-			churchtwoaudio.play();
-		}).mouseleave(function() {
-			churchtwoaudio.pause();
-			churchtwoaudio.currentTime = 0;
-		});
-	//stadium 1
-		var stadoneTimeout
-		var stadoneaudio = $("#stadium-1")[0];
-		$(".stadium-1").mouseenter(function() {
-			stadoneTimeout = setTimeout(function() {
-				stadoneaudio.play();
-			}, 800);
-		}).mouseleave(function() {
-			stadoneaudio.pause();
-			stadoneaudio.currentTime = 0;
-			clearTimeout(stadoneTimeout);
-		});
-	//stadium 2
-		var stadtwoTimeout
-		var stadtwoaudio = $("#stadium-2")[0];
-		$(".stadium-2").mouseenter(function() {
-			stadtwoTimeout = setTimeout(function() {
-				stadtwoaudio.play();
-			}, 800);
-		}).mouseleave(function() {
-			stadtwoaudio.pause();
-			stadtwoaudio.currentTime = 0;
-			clearTimeout(stadtwoTimeout);
-		});
-	//police hell
-		var hellTimeout
-		var hellaudio = $("#hell")[0];
-		$(".airport").mouseenter(function() {
-			hellTimeout = setTimeout(function() {
-				hellaudio.play();
-			}, 800);
-		}).mouseleave(function() {
-			hellaudio.pause();
-			hellaudio.currentTime = 0;
-			clearTimeout(hellTimeout);
-		});
-	//prisoner youhoo
-		var youhooaudio = $("#youhoo")[0];
-		$(".prisoner").mouseenter(function() {
-			youhooaudio.play();
-		}).mouseleave(function() {
-			youhooaudio.pause();
-			youhooaudio.currentTime = 0;
-		});
-	//severed body parts
-		var bodyaudio = $("#severed-foot")[0];
-		$(".tt-wrapper.foot-info, .tt-wrapper.foot-2, .tt-wrapper.hand-1, .tt-wrapper.hand-2, .tt-wrapper.head").mouseenter(function() {
-			bodyaudio.play();
-		}).mouseleave(function() {
-			bodyaudio.pause();
-			bodyaudio.currentTime = 0;
-		});
+
+
 	//rainforest
 		var forestaudio = $("#rainforest")[0];
 		$(".rainforest").mouseenter(function() {
@@ -259,17 +181,7 @@ jQuery(document).ready(function ($) {
 			forestaudio.pause();
 			forestaudio.currentTime = 0;
 		});
-	//run for your life
-		var runaudio = $("#runforyourlife")[0];
-		$(".homicide").mouseenter(function() {
-			runaudio.play();
-			runaudio.volume = 0.5;
-		}).mouseleave(function() {
-			runaudio.pause();
-			runaudio.currentTime = 0;
-			runaudio.volume = 0.5;
-		});
-
+	
 	//mute un-mute all sounds
 	$(".mute").click(function() {
 		var bool = $(".player").prop("muted");
